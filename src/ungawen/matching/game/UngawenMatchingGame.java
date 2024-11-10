abe2fde113dc54bbcdf2528adc9c96e0594ed940
cjmagdangan <159784711+cjmagdangan@users.noreply.github.com>
2024-11-10 12:21:41 +0800

    Add files via upload

---
+++ src/ungawen/matching/game/UngawenMatchingGame.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ungawen.matching.game;
+
+/**
+ *
+ * @author cj
+ */
+public class UngawenMatchingGame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new ungawenWelcome().setVisible(true);
+    }
+    
+}
